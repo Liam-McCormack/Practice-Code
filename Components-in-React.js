da05bc9e7776205da74d739b07fe5978ac2b053a
Liam <liammc12@hotmail.co.uk>
2020-07-16 11:07:53 +0100

    updates

--- Components-in-React.js
+++ Components-in-React.js
@@ -140,3 +140,105 @@ ReactDOM.render(
   <Friend/>,
   document.getElementById('app')
 );
+
+
+
+
+
+
+//USING A CONDITIONAL IN A RENDER FUNCTION
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+class TonightsPlan extends React.Component {
+  render() {
+    if (fiftyFifty) {
+      return <h1>Tonight I'm going out WOOO</h1>
+    } else {
+      return <h1>Tonight I'm going to bed WOOO</h1>
+    }
+  }
+}
+
+ReactDOM.render(
+  <TonightsPlan />,
+  document.getElementById('app')
+);
+
+//another example
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TodaysPlan extends React.Component {
+  render() {
+    let task;
+    if (!apocalypse) {
+      task = 'learn React.js'
+    } else {
+      task = 'run around'
+    }
+
+    return <h1>Today I am going to {task}!</h1>;
+  }
+}
+
+ReactDOM.render(
+	<TodaysPlan />,
+	document.getElementById('app')
+);
+
+
+
+
+
+
+//THIS KEYWORD IN A REACT COMPONENT
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyName extends React.Component {
+	// name property goes here:
+  get name() { 
+    return 'Liam';
+  }
+  render() {
+    return <h1>My name is {this.name}.</h1>; //Don't need to include parentheses with 'this.name' as it is a Getter.
+  }
+}
+
+ReactDOM.render(<MyName />, document.getElementById('app'));
+
+
+
+
+
+
+
+//EVENT LISTENERS IN COMPONENTS - we define event handlers as methods on a component class.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Button extends React.Component {
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
+  }
+
+  warning() {
+    alert('This is a warning to not click here!');
+  }
+
+  render() {
+    return ( //dont forget these parentheses!
+    <button 
+    onMouseOver={this.warning}
+    onClick={this.scream}>AAAAAH!</button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Button/>,
+  document.getElementById('app')
+);
