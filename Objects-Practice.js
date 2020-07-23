dd5c25a25ef508fad1ccbd4d00742450db5193e9
Liam <liammc12@hotmail.co.uk>
2020-07-23 12:42:07 +0100

    some more practice

--- Objects-Practice.js
+++ Objects-Practice.js
@@ -386,3 +386,31 @@ Object.assign(newRobot, robot)//Assigns the KEYS and VALUES of robot to newRobot
 console.log(newRobot); */
 
 
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+  // Only change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));
